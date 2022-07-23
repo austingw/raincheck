4db99d2dbf63ed84a246777b6206714774dedeae
Austin Williams <senortinman@gmail.com>
2022-07-23 16:10:21 -0400

    Add Weather Data parse function. City, current temp, and feels like data all display on page

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const btn = document.getElementById("formBtn");
+let units = "imperial";
 
 btn.addEventListener("click", (e) => {
   e.preventDefault();
@@ -11,12 +12,47 @@ btn.addEventListener("click", (e) => {
 async function getWeather(location) {
   try {
     const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=400ad950e4bd6409fef17da6c80067a2`,
+      `https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=400ad950e4bd6409fef17da6c80067a2&units=${units}`,
       { mode: "cors" }
     );
     const weatherData = await response.json();
-    console.log(weatherData);
+
+    //console.log(weatherData);
+    parseWeatherData(weatherData);
   } catch (e) {
     console.log(e);
   }
 }
+
+function parseWeatherData(weatherData) {
+  let tempUnit = "°F";
+
+  //if (units === "metric") {
+  //  tempUnit = "°C";
+  // }
+
+  const location = document.querySelector("#location-title");
+  location.textContent = `${weatherData.name}, ${weatherData.sys.country}`;
+
+  const temp = document.querySelector("#current-weather");
+  const tempMax = document.querySelector("#hi-temp");
+  const tempMin = document.querySelector("#lo-temp");
+  const feelsLike = document.querySelector("#feels-like");
+
+  temp.textContent = `${Math.round(weatherData.main.temp)} ${tempUnit}`;
+
+  feelsLike.textContent = `Feels like: ${Math.round(
+    weatherData.main.feels_like
+  )} ${tempUnit}`;
+  /*tempMax.textContent = `H: ${Math.round(
+    weatherData.daily.temp.max
+  )} ${tempUnit}`;
+  tempMin.textContent = `L: ${Math.round(
+    weatherData.main.temp_min
+  )} ${tempUnit}`;
+
+  let humidity = weatherData.main.humidity;*/
+}
+
+//default location*/
+getWeather("Chesapeake");
